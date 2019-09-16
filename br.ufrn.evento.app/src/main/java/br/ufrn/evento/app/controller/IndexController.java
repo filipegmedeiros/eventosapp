10788c27ed602e8647f9990112f21fc2ee2f9f0b
Filipe Medeiros <filipemedeiros@tuta.io>
2019-09-16 09:37:34 -0300

    Remodelando para MVC criando primeiro form
    Criando cadastrarEvento  usando @Controller e seu template html

--- br.ufrn.evento.app/src/main/java/br/ufrn/evento/app/controller/IndexController.java
+++ br.ufrn.evento.app/src/main/java/br/ufrn/evento/app/controller/IndexController.java
@@ -1,4 +1,4 @@
-package br.ufrn.evento.app;
+package br.ufrn.evento.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/") /* Requisao, retorno */
     public String index(){
         return "index";
     }
